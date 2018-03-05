cbc66234d0b5c0be749d9505b592cc7982accf6b
maklaos <maklaosdelay@gmail.com>
2018-03-05 21:52:36 +0200

    sceleton

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+    <title>Missionred</title>
+    <meta charset='utf-8'>
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+<div id="wrapper">
+	<canvas id='game' width='600' height='600'>Element not supported</canvas>
+</div>
+
+<script type="text/javascript" src="game.js"></script>
+
+</body>
+</html>
